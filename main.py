6918ea79cc6a638f7a4a8ba060a951a43962ac4c
henriquefirbidagirotto-cmd <henriquefirbidagirotto@gmail.com>
2026-05-19 16:50:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.controllers.main import router
 from app.models.database import init_db
+import streamlit as st
+from database import inicializar_banco
 
 app = FastAPI(title="API Concessionaria Digital")
 
